a67da6de7eb1d41cc5dcb0b4387a4a1150686d46
Florian Uekermann <florian@uekermann.me>
2017-08-08 19:37:00 +0200

    Install genny with go generate

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
 package critbit
 
+//go:generate go get github.com/cheekybits/genny
 //go:generate genny -in integer/map.go -out integerMaps.go gen "KeyType=int,int64,int32,int16,int8,uint,uintptr,uint64,uint32,uint16,uint8 ValueType=BUILTINS"
